6fdc95d44921f6241b2a3e9f68e3cb556938f2ac
Duy Chu <duycs@outlook.com>
2023-02-06 17:28:19 +0700

    Update and rename shuffle-array to shuffle-array.cs

--- shuffle-array.cs
+++ shuffle-array.cs
@@ -1,9 +1,9 @@
-### Shuffle the Array
+/* Shuffle the Array
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+*/
 
-### C# implement
- public int[] Shuffle(int[] nums, int n) {
+public int[] Shuffle(int[] nums, int n) {
         var result = new List<int>();
         var numsLeft = new List<int>();
         var numsRight = new List<int>();
